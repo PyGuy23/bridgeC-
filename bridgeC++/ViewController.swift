5f79e36fd2954b76256d9464271dae77147479af
Timothy <eclisesolaris23@gmail.com>
2016-04-24 12:27:33 -0400

    Initial Commit

---
+++ bridgeC++/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bridgeC++
+//
+//  Created by newuser on 4/24/16.
+//  Copyright © 2016 dwebman235. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
